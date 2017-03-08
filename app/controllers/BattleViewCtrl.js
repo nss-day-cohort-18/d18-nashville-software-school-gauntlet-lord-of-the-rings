eed69c53da769b7c5ea560f6528a3fa852aba384
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-03-08 11:41:45 -0600

    Set up angular instead of browserify

---
+++ app/controllers/BattleViewCtrl.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log("BattleViewCtrl.js is connected!!");
+
+app.controller("BattleViewCtrl", function($scope) {
+	let s = $scope;
+
+		
+});
---
+++ app/factories/LevelsFactory.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("LevelsFactory.js is connected");
+
+app.factory("LevelsFactory", function() {
+
+});
