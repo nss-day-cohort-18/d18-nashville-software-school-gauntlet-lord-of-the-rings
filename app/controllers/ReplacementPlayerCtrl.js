cd9a25c726745cbeb1b82dec022e4e45b7d8fef6
Casey Dailey <casey@feasttogether.org>
2017-03-09 10:55:26 -0600

    added a dummy controller for replacement player modal

---
+++ app/controllers/ReplacementPlayerCtrl.js
@@ -0,0 +1,8 @@
+"use strict";
+
+console.log("ReplacementPlayerCtrl.js is connected!!");
+
+app.controller("ReplacementPlayerCtrl", function($scope, CurrentStateFactory, LevelsFactory) {
+	let s = $scope;
+		
+});
